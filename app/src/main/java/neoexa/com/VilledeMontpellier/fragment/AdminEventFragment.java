e7ba0d1fc065d4577a8fd939cbd210490ea9e1ed
neoexa <mmdbareche@gmail.com>
2018-05-23 09:57:35 +0200

    events RecyclerView, calendar intent

--- app/src/main/java/neoexa/com/VilledeMontpellier/fragment/AdminEventFragment.java
+++ app/src/main/java/neoexa/com/VilledeMontpellier/fragment/AdminEventFragment.java
@@ -4,14 +4,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
 import neoexa.com.VilledeMontpellier.AddEventActivity;
+import neoexa.com.VilledeMontpellier.Model.Event;
 import neoexa.com.VilledeMontpellier.R;
+import neoexa.com.VilledeMontpellier.viewholder.EventViewHolder;
 
 public class AdminEventFragment extends Fragment implements View.OnClickListener{
+    private String uid;
+    private DatabaseReference mDatabase;
+    private FirebaseRecyclerAdapter<Event, EventViewHolder> mAdapter;
+    private RecyclerView mRecycler;
+    private LinearLayoutManager mManager;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -22,7 +38,75 @@ public class AdminEventFragment extends Fragment implements View.OnClickListener
 
 
 
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mRecycler = (RecyclerView) rootView.findViewById(R.id.admin_events_list);
+        mRecycler.setHasFixedSize(true);
+
+        mManager = new LinearLayoutManager(getActivity());
+        mManager.setReverseLayout(true);
+        mManager.setStackFromEnd(true);
+        mRecycler.setLayoutManager(mManager);
+
+
+        // Set up FirebaseRecyclerAdapter with the Query
+
+        Query eventsQuery = mDatabase.child("events").limitToFirst(1);
+
+        FirebaseRecyclerOptions options = new FirebaseRecyclerOptions.Builder<Event>()
+                .setQuery(eventsQuery, Event.class)
+                .build();
+
+        mAdapter = new FirebaseRecyclerAdapter<Event, EventViewHolder>(options) {
+
+            @Override
+            public EventViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+                LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+                return new EventViewHolder(inflater.inflate(R.layout.item_event, viewGroup, false));
+            }
+            @Override
+            protected void onBindViewHolder(EventViewHolder viewHolder, int position, final Event model) {
+                final DatabaseReference eventRef = getRef(position);
+                // Set click listener for the whole event view
+                viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //todo
+                    }
+                });
+
+                // Bind Event to ViewHolder, setting OnClickListener
+                viewHolder.bindToEvent(model, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View cardViewBtn) {
+                        //todo
+                    }
+                });
+            }
+        };
+        mRecycler.setAdapter(mAdapter);
+
+
         return rootView;
+    }
+
+
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (mAdapter != null) {
+            mAdapter.startListening();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAdapter != null) {
+            mAdapter.stopListening();
+        }
     }
 
     @Override
--- app/src/main/java/neoexa/com/VilledeMontpellier/fragment/AdminShopsFragment.java
+++ app/src/main/java/neoexa/com/VilledeMontpellier/fragment/AdminShopsFragment.java
@@ -8,24 +8,82 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 import neoexa.com.VilledeMontpellier.AddShopActivity;
+import neoexa.com.VilledeMontpellier.Model.Shop;
+import neoexa.com.VilledeMontpellier.Model.ShopAdapter;
 import neoexa.com.VilledeMontpellier.R;
+import neoexa.com.VilledeMontpellier.ShopsActivity;
 
 public class AdminShopsFragment extends Fragment implements View.OnClickListener {
 
     FloatingActionButton addShop;
+    private String uid;
+    private DatabaseReference mDatabase;
+
+    private ListView shopsListView;
+    private ArrayList<Shop> myShops = new ArrayList<>();
+    private ShopAdapter adapter;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_admin_shops, container, false);
 
+        //Current user
+        FirebaseUser authUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (authUser != null) {
+            uid = authUser.getUid();
+        } else {
+            Toast.makeText(getActivity(), "Erreur utilisateur non connecté !",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        //Database
+        mDatabase = FirebaseDatabase.getInstance().getReference("shops");
+
+
         //Views
         rootView.findViewById(R.id.fab_add_shop).setOnClickListener(this);
+        shopsListView = (ListView) rootView.findViewById(R.id.adminshoplv);
 
         //Events
+        //Listener on DB
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
 
+                myShops.clear();
+
+                for (DataSnapshot shopsSnapshot : dataSnapshot.getChildren()) {
+                    Shop retrievedShop = shopsSnapshot.getValue(Shop.class);
+                    if (retrievedShop.userId == uid){
+                        myShops.add(retrievedShop);}
+                }
+
+                //Adapteur
+                adapter = new ShopAdapter(getActivity(), myShops, myShops);
+                shopsListView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         return rootView;
     }
